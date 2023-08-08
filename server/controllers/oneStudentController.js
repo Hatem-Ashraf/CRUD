a3b9129b87465934bb0abb9ae2a23cb633ce2326
ranatarek-rt <rana195387@feng.bu.edu.eg>
2023-08-08 22:39:33 +0300

    student result page

--- server/controllers/oneStudentController.js
+++ server/controllers/oneStudentController.js
@@ -1,19 +1,20 @@
-const Student = require('../models/Students')
+const stuControl = require('../models/studentsControl');
 
 const getOneStudent = async (req, res) => {
     try {
-        const bd = req.body
-        const students = await Student.findOne({ _id: req.params.id }).exec()
+        const student = await stuControl.findOne({ _id: req.params.id }).exec();
 
-        if (!students) return res.status(400).send(`No student found for this ID :${req.params.id}`)
+        if (!student) return res.status(400).send(`No student found for this ID: ${req.params.id}`);
 
-        res.render('students/StudentResult', students)
+        res.render('students/StudentResult', { student }); // Pass an object with the key 'student'
 
     } catch (err) {
-        console.error(err)
-        res.status(500).send("Internal server error")
+        console.error(err);
+        res.status(500).send("Internal server error");
     }
-}
+};
+
+
 
 module.exports = {
     getOneStudent
