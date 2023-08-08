7991a0265531d18fe91eb4c2e3659435c07b96cf
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2023-08-08 11:47:45 -0700

    Student login

--- server/routes/studentControl.js
+++ server/routes/studentControl.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const stucontController = require('../controllers/stuContController');
+
+
+
 router.post('/uploadgrades', stucontController.createNewStudent);
-router.get('/update/:id', stucontController.update);
 
 
-router.route('/')
-    // .get(stucontController.getAllStudents)
-    .post(stucontController.createNewStudent)
 
-router.route("/update/:id")
-    // .get(stucontController.getOneStudent)
-    .put(stucontController.update)
-    // .delete(stucontController.deleteStudent)
 
 module.exports = router
